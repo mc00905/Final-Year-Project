3d69da5e08fa19116cb571070db180cf1bc68b96
Matt Cooper <matthew.cooper@btinternet.com>
2021-02-15 18:52:52 +0000

    Add response status codes for delete and put requests

--- src/service-layer/controllers/ExampleController.ts
+++ src/service-layer/controllers/ExampleController.ts
@@ -1,10 +1,11 @@
 
-import { Body, Delete, Get, Path, Post, Put, Route } from 'tsoa';
+import { Body, Delete, Get, Path, Post, Put, Route, SuccessResponse } from 'tsoa';
 import { Example } from '../../data-layer/model-types/ExampleType'
 import { createOrUpdateExample, getExample, getExamples, deleteExample } from '../../data-layer/data-agents/ExampleAgent';
 
 @Route("1.0/examples")
 export class ExampleController {
+    @SuccessResponse("201", "Created")
     @Post("")
     public async createExample (@Body() body: Example): Promise<void> {
         await createOrUpdateExample(body.name, body.description);
@@ -24,7 +25,7 @@ export class ExampleController {
     public async getExample (@Path() name: string): Promise<Example> {
         return await getExample(name);    
     }
-
+    @SuccessResponse("204", "Deleted")
     @Delete("/{name}")
     public async deleteExample (@Path() name: string): Promise<void> {
         await deleteExample(name);
