d4f77a99ca7b40709ab55278877ba3d855a1347a
Matt Cooper <matthew.cooper@btinternet.com>
2021-02-16 16:34:23 +0000

    Add basic rate limiting support

--- App.ts
+++ App.ts
@@ -1,11 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { RegisterRoutes } from "./src/service-layer/Routes/routes";
+import express from 'express';
+import bodyParser from 'body-parser';
+import { RegisterRoutes } from './src/service-layer/Routes/routes';
+import { rateLimiter } from './src/middleware/RateLimiter'
 import  * as dotenv from 'dotenv';
 import mongoose = require("mongoose");
 
 dotenv.config();
 export const app = express();
+app.use(rateLimiter)
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -13,6 +15,6 @@ app.use(
   })
 );
 const config = (async () => {
-  await mongoose.connect("mongodb://localhost/example-database", {useNewUrlParser: true, useUnifiedTopology: true})
+  await mongoose.connect("mongodb://localhost/shopping-items-database", {useNewUrlParser: true, useUnifiedTopology: true})
 })();
-RegisterRoutes(app);  
+RegisterRoutes(app);
