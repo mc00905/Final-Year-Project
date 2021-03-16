ad042a6e9323bd1bff7681008dfb7509391c02bb
Matt Cooper <matthew.cooper@btinternet.com>
2021-03-16 21:23:01 +0000

    Fix minor hangup issue around tests

--- microservice/tests/ShoppingItemController.spec.ts
+++ microservice/tests/ShoppingItemController.spec.ts
@@ -1,13 +1,17 @@
 import { Server } from 'node:http';
 import supertest from 'supertest';
+import mongoose = require("mongoose");
+
 import app from '../src/App';
 
-const request = supertest(app);
 
 describe('A test', () => {
-
-    it('A test', async () => {
-        return request.get('/REST/1.0/shoppingItems')
+    afterAll(async () => {
+        await mongoose.disconnect()
+    })
+    it('GET /shoppingItems', async () => {
+        const request = supertest.agent(app);
+        await request.get('/REST/1.0/shoppingItems')
         .expect(200)
     })
 })
